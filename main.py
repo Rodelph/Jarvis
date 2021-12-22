febd8541702030fdb47edf3eec10b09e8f8b93b1
Rodelph <anaqi@um6ss.ma>
2021-12-22 18:50:35 +0100

    added search function

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import pyttsx3 as jarv
 import datetime
 import speech_recognition as sr
 import pywhatkit as kit
+import re
 
 engine = jarv.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 r = sr.Recognizer()
+Time = datetime.datetime.now()
+
+def searchGoogle(querySearch):
+    kit.search(querySearch)
+
+def findWholeWord(w):
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
 def speak(audio):
     engine.say(audio)
@@ -14,7 +22,7 @@ def speak(audio):
 
 def time():
     Time = datetime.datetime.now().strftime("%I Hours : %M Minutes : %S Seconds")
-    speak(Time)
+    speak("It is currently " + str(Time))
 
 def date():
     year = int(datetime.datetime.now().year)
@@ -22,34 +30,54 @@ def date():
     day = int(datetime.datetime.now().day)
     speak("" + str(day) + " " + str(month) + " " + str(year))
 
-def morningGreeting():
-    speak("Good morning sir !")
-    speak("The current time is ")
-    time()
-    speak("and the current day is")
-    date()
-    speak("Do you need anything Sir ?")
+def greeting():
+    speak("Good morning sir ! What can i do for you ?")
 
-def playMusic(audioMusic):
-    kit.playonyt(audioMusic)
+def greetingBack():
+    speak("Greetings sir you seem cheerful today !")
 
 def voiceMusic():
  with sr.Microphone() as sourceMusic :
-                speak("What type of music Sir ?")
                 print("Recognizing song name...")
                 r.pause_threshold = 1
                 audioMusic = r.listen(sourceMusic)
+
                 try :
                     queryMusic = str(r.recognize_google(audioMusic, language="en-UK"))
                     print(queryMusic)
-                    playMusic(queryMusic)
+                    speak("Playing " + queryMusic + " !")
+                    kit.playonyt(queryMusic)
                 except Exception as e:
                     print(e)
-                    speak("Couldn't hear the song name  Sir !")
+                    speak("Couldn't hear the song name  Sir ! Could you repeat again ?")
                     voiceMusic()
 
-def recog():
 
+def sendMessageWtsp():
+    minute = Time.minute + 1
+    kit.sendwhatmsg("+212632637463",
+                                    "Hello",
+                                    Time.hour, minute,
+                                    tab_close=True)
+
+def searchVoice():
+    with sr.Microphone() as sourceSearch :
+        print("Listening...")
+        
+        r.pause_threshold = 1
+        audioSearch = r.listen(sourceSearch)
+
+        try:
+            querySearch= str(r.recognize_google(audioSearch, language="en-UK"))
+            print(querySearch)
+            speak("Searching for " + querySearch + " !")
+            searchGoogle(querySearch)
+        except Exception as e:
+            print(e)
+            speak("Couldn't understand what you meant Sir ! Could you repeat again ?")
+            searchVoice()
+
+def recog():
     with sr.Microphone() as source :
         print("Listening...")
         r.pause_threshold = 1
@@ -59,14 +87,26 @@ def recog():
         print("Recognizing...")
         query = str(r.recognize_google(audio, language="en-UK"))
         print(query)
-        if query.lower() == "what's today's date" or query.lower() == "what is today's date" :
+        
+        if findWholeWord("date")(query.lower()):
             date()
 
-        if query.lower() == "play music":
+        if findWholeWord("music")(query.lower()):
+            speak("What type of music Sir ?")
             voiceMusic()
 
-        if query.lower() == "hello jarvis":
-            morningGreeting()
+        if findWholeWord("time")(query.lower()) :
+            time()
+
+        if findWholeWord("hello")(query.lower()):
+            greetingBack()
+
+        if findWholeWord("search")(query.lower()):
+            speak("What are you looking for Sir ?")
+            searchVoice()
+
+        if  findWholeWord("message")(query.lower()):
+            sendMessageWtsp()
 
     except Exception as e:
         print(e)
@@ -77,4 +117,6 @@ def recog():
     return query
 
 if __name__ == "__main__":
-    recog()
+    greeting()
+    while(True):
+        recog()
